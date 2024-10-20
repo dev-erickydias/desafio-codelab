c0e1445a5d464bf347e806ceaa47b5c613886bf1
Ericky Dias <erickyhenriquesd@gmail.com>
2024-10-20 22:07:03 +0100

    redefini log

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
     return (
         <div className="hero">
-            <img src="/logo" alt="Logo" />
+            <img src="/logo.svg" alt="Logo" />
             <h1 className="hero__title">A melhor loja Jordan</h1>
             <p className="hero__text">O tênis Jordan é fruto de uma velha e forte parceria entre a nike e o jogador Michael Jhordan.</p>
         </div>
