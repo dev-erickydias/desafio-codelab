73559bb05f767f978509cd9378a3a1dd2c1f7e77
Ericky Dias <erickyhenriquesd@gmail.com>
2024-10-20 22:15:15 +0100

    add fonts no html

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,9 +6,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body>
-        {children}
-      </body>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{metadata.title}</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Archivo:wght@500&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
