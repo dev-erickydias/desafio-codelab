180ea6f4020707fefd7a5a511e17340534ebc9ac
Lucas_Pasa <lucaspasagarcia29@gmail.com>
2024-10-20 22:23:09 +0100

    style the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
-export default function Header(){
-    return(
-        <div className="header">
-            <h1 className="header__title">Frete Grátis para todo o Brasil</h1>
-        </div>
-    )
+import "../blocks/header.css";
+export default function Header() {
+  return (
+    <div className="header">
+      <h1 className="header__title">Frete Grátis para todo o Brasil</h1>
+    </div>
+  );
 }
