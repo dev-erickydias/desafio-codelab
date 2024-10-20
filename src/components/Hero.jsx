bbf93f7314e39d7eee89b89260e8d49def8ae1dc
Ericky Dias <erickyhenriquesd@gmail.com>
2024-10-20 22:04:23 +0100

    add hero container

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <div className="hero">
+            <img src="/logo" alt="Logo" />
+            <h1 className="hero__title">A melhor loja Jordan</h1>
+            <p className="hero__text">O tênis Jordan é fruto de uma velha e forte parceria entre a nike e o jogador Michael Jhordan.</p>
+        </div>
+    )
+}
